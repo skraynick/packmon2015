163eb467f9df595fd2ad86ebf67580e58769ea32
Georgi Alipiev <Georgi.Alipiev@outlook.com>
2015-09-26 23:37:53 +0200

    RabbitMQ added to the project and reading data.

---
+++ app/src/main/java/de/packmon/Values.java
@@ -0,0 +1,12 @@
+package de.packmon;
+
+/**
+ * Created by georg_000 on 26/09/2015.
+ */
+public final class Values {
+
+    public static final String SERVER_ADDRESS = "192.168.77.87";
+    public static final String EXCHANGE_NAME = "packet.monitor";
+    public static final String QUEUE_NAME = "packet.monitor";
+
+}
