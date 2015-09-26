beab21a32a4708ac4361080daaee4cd860e82d59
sarah <srkraynick@gmail.com>
2015-09-26 21:40:51 +0200

    map is working. yeah!

--- app/src/main/java/de/packmon/HomeActivity.java
+++ app/src/main/java/de/packmon/HomeActivity.java
@@ -1,28 +1,57 @@
 package de.packmon;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
 
 public class HomeActivity extends FragmentActivity {
 
     private Fragment homeActivityFragment;
     private Fragment shipmentItemInformationFragment;
     private FragmentTabHost mTabHost;
+    // map embedded in the map fragment
+    private Map map = null;
+    // map fragment embedded in this activity
+    private MapFragment mapFragment = null;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        
-        if (savedInstanceState == null) {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+
+            // Search for the map fragment to finish setup by calling init().
+            mapFragment = (MapFragment)getFragmentManager().findFragmentById(
+                    R.id.mapfragment);
+            mapFragment.init(new OnEngineInitListener() {
+                @Override
+                public void onEngineInitializationCompleted(
+                        OnEngineInitListener.Error error)
+                {
+                    if (error == OnEngineInitListener.Error.NONE) {
+                        // retrieve a reference of the map from the map fragment
+                        map = mapFragment.getMap();
+                        // Set the map center to the Vancouver region (no animation)
+                        map.setCenter(new GeoCoordinate(52.5167, 13.3833, 0.0),
+                                Map.Animation.NONE);
+                        // Set the zoom level to the average between min and max
+                        map.setZoomLevel(
+                                (map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+                    } else {
+                        System.out.println("ERROR: Cannot initialize Map Fragment");
+                    }
+                }
+            });
+        }
+
+
+   /*     if (savedInstanceState == null) {
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
             HomeActivityFragment fragment = new HomeActivityFragment();
             transaction.replace(R.id.fragment_container, fragment);
@@ -47,6 +76,6 @@ public class HomeActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
+*/
 
 }
--- app/src/main/java/de/packmon/HomeActivityFragment.java
+++ app/src/main/java/de/packmon/HomeActivityFragment.java
@@ -1,10 +1,24 @@
 package de.packmon;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Retrofit;
+import retrofit.http.GET;
+import retrofit.http.Path;
 
 
 /**
@@ -12,12 +26,80 @@ import android.view.ViewGroup;
  */
 public class HomeActivityFragment extends Fragment {
 
+    private View view;
+    public static final String BASE_URL = "http://{1-4}.base.maps.api.here.com";
+
+
+
     public HomeActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .build();
+
+        GitHubService service = retrofit.create(GitHubService.class);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        showMap(view);
+        return view;
+    }
+
+    public interface GitHubService {
+        @GET("/users/{user}/repos")
+        Call<List<String>> listRepos(@Path("skraynick") String user);
+    }
+
+    private void showMap(View view) {
+        try {
+            //URL url = new URL("http://2.base.maps.cit.api.here.com/maptile/2.1/basetile/newest/normal.day/14/4769/6203/256/png8?app_id=&app_code=c_rjgR49ZG44tUJodLsYDw");
+            URL url = new URL("https://www.google.de/maps");
+            TextView viewer = (TextView) view.findViewById(R.id.map);
+            viewer.setText(url.toString());
+
+
+        } catch (Exception ex) {
+            System.out.print(ex);
+
+        }    }
+
+
+    private class RenderMapAPI extends AsyncTask<String, String, String> {
+
+        //String urlString=params[0];
+        String resultToDisplay;
+        //emailVerificationResult result = null;
+        InputStream in = null;
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            // HTTP Get
+           try {
+               URL url = new URL("http://2.base.maps.cit.api.here.com/maptile/2.1/basetile/newest/normal.day/14/4769/6203/256/png8?app_id=&app_code=c_rjgR49ZG44tUJodLsYDw");
+               HttpURLConnection connection  = (HttpURLConnection) url.openConnection();
+
+               InputStream is = connection.getInputStream();
+               Bitmap img = BitmapFactory.decodeStream(is);
+
+               TextView view = (TextView) getActivity().findViewById(R.id.map);
+               view.setText(url.toString());
+           } catch (Exception ex) {
+               System.out.print(ex);
+
+            }
+
+            return "";
+        }
+
+        protected void onPostExecute(String result) {
+            //Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+           // intent.putExtra(EXTRA_MESSAGE, result);
+           // startActivity(intent);
+        }
     }
 }
