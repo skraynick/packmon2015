42d7b9cfc7ba6f91690e0e7415be354c1a4061cf
sarah <srkraynick@gmail.com>
2015-09-26 16:41:31 +0200

    stuff

--- app/src/main/java/de/packmon/HomeActivity.java
+++ app/src/main/java/de/packmon/HomeActivity.java
@@ -1,39 +1,52 @@
 package de.packmon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+
+public class HomeActivity extends FragmentActivity {
+
+    private Fragment homeActivityFragment;
+    private Fragment shipmentItemInformationFragment;
+    private FragmentTabHost mTabHost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            HomeActivityFragment fragment = new HomeActivityFragment();
+            transaction.replace(R.id.fragment_container, fragment);
+            transaction.commit();
+        }
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home, menu);
         return true;
     }
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
 
+        return super.onPrepareOptionsMenu(menu);
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch(item.getItemId()) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
--- app/src/main/java/de/packmon/ShipmentInformationFragment.java
+++ app/src/main/java/de/packmon/ShipmentInformationFragment.java
@@ -10,14 +10,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class HomeActivityFragment extends Fragment {
+public class ShipmentInformationFragment extends Fragment {
 
-    public HomeActivityFragment() {
+    public ShipmentInformationFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_parcel_info, container, false);
     }
 }
